078db574c52d7f7709f61a3d5ab81a1cbc170989
AbnerMendoza <abnerjmendoza@gmail.com>
2022-07-11 19:08:48 -0400

    produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBs8dZ2m8bXwFw6hCIv_8h9wFXzGsaCwpg",
+    authDomain: "pruebas-1afa0.firebaseapp.com",
+    databaseURL: "https://pruebas-1afa0.firebaseio.com",
+    projectId: "pruebas-1afa0",
+    storageBucket: "pruebas-1afa0.appspot.com",
+    messagingSenderId: "94637205150",
+    appId: "1:94637205150:web:c2c201b137e619e92b7d8f",
+    measurementId: "G-8W6CDMTCZQ"
+  }
 };
